d641795bae89515a036dc3f7b72d9601806fdfe5
ココヤマ <65098171+ko2ym@users.noreply.github.com>
2020-05-20 23:52:15 +0900

    Update tweetimgsplit.py

--- tweetimgsplit.py
+++ tweetimgsplit.py
@@ -2,37 +2,43 @@
 import cv2
 import sys
 # 処理する画像ファイル名を定義
-#BaseFile = sys.argv[1] + '.jpg'
-BaseFile = sys.argv[1] + '.png'
+BaseFile = sys.argv[1] + '.jpg'
+#BaseFile = sys.argv[1] + '.png'
 # 書き出すファイル名を定義
 MakeFile = [ 0 for i in range(5) ]
 for i in range(1,5):
-#  MakeFile[i] = sys.argv[1] + '_' + str(i) + '.jpg'
-  MakeFile[i] = sys.argv[1] + '_' + str(i) + '.png'
+  MakeFile[i] = sys.argv[1] + '_' + str(i) + '.jpg'
+#  MakeFile[i] = sys.argv[1] + '_' + str(i) + '.png'
 # 画像の読み出し
 ImgSrc = cv2.imread(BaseFile)
 # 画像のサイズを取得
 ymax, xmax, channels = ImgSrc.shape[:3]
 # 画像のサイズが分割可能か確認
+print("4:3 " + str(xmax) + ":" + str(ymax))
 try:
   # 16:9で処理する
   x9 = xmax // 16
-  ymax = x9 * 9
+  _ymax = x9 * 9
+  # 16:9の場合y軸をずらす
+  shiftpx = ( ymax - _ymax ) // 2
+  print("shiftpx: " + str(shiftpx))
   # 座標を求める
-  yzero = 0
+  yzero = 0 + shiftpx
   xzero = 0
-  ydiv = ymax // 2
+  # 16:9の場合y軸をずらす
+  ydiv = ( _ymax // 2 ) + shiftpx
   xdiv = xmax // 2
+  # 16:9の場合y軸をずらす
+  _ymax = _ymax + shiftpx
 except ZeroDivisionError:
   print("画像が分割できませんでした")
 else:
   # トリミングを行う
-#  print("width: " + str(xmax))
-#  print("height: " + str(ymax))
+  print("16:9 " + str(xmax) + ":" + str(_ymax))
   Img1 = ImgSrc[yzero:ydiv,xzero:xdiv, :]
   Img2 = ImgSrc[yzero:ydiv,xdiv:xmax, :]
-  Img3 = ImgSrc[ydiv:ymax,xzero:xdiv, :]
-  Img4 = ImgSrc[ydiv:ymax,xdiv:xmax, :]
+  Img3 = ImgSrc[ydiv:_ymax,xzero:xdiv, :]
+  Img4 = ImgSrc[ydiv:_ymax,xdiv:xmax, :]
   # 画像を保存
   cv2.imwrite(MakeFile[1],Img1)
   cv2.imwrite(MakeFile[2],Img2)
